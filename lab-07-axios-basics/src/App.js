882a11c482d3f8812220f4ec49c447c27fdd462b
kunxin-chor <chorkunxin@yahoo.com>
2021-10-01 06:39:10 +0000

    Added axios examples

---
+++ lab-07-axios-basics/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FruitList from './FruitList'
+
+function App() {
+  return (
+   <React.Fragment>
+     <FruitList/>
+   </React.Fragment>
+
+  );
+}
+
+export default App;
