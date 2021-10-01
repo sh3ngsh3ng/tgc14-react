c686c6dd6740fee0cb7de1e4a3df6672e27b9f19
kunxin-chor <chorkunxin@yahoo.com>
2021-10-01 08:16:54 +0000

    Modify task

---
+++ lab-08-todolist/src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import Todolist from './Todolist';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Todolist/>
+    </React.Fragment>
+  );
+}
+
+export default App;
