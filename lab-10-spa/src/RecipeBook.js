389ebe153088d7d3ef0510aaea7bc9512a55b5ee
kunxin-chor <chorkunxin@yahoo.com>
2021-10-05 09:22:24 +0000

    Added add new recipe

--- lab-10-spa/src/RecipeBook.js
+++ lab-10-spa/src/RecipeBook.js
@@ -4,8 +4,41 @@ import Listing from './components/Listing'
 export default class RecipeBook extends React.Component {
 
     state = {
-        'active':'listing' // if `active` variable is 'listing', then we show the listing component
-                           // if `active` variable is 'addnew', then we show the add new component
+        // if `active` variable is 'listing', then we show the listing component
+        // if `active` variable is 'addnew', then we show the add new component
+        'active': 'listing',
+        'data': [
+            {
+                '_id':1,
+                'title':'Chicken Rice',
+                'ingredients':[
+                    'Chicken Broth',
+                    'Rice',
+                    'Chicken'
+                ]
+            },
+            {
+                '_id':2,
+                'title':'Duck Rice',
+                'ingredients':[
+                    'Duck',
+                    'Rice',
+                    'Soya Sauce'
+                ]
+            },
+            {
+                '_id':3,
+                'title':'Roti Prata',
+                'ingredients':[
+                    'Flour',
+                    'Egg',
+                    'Sugar',
+                    'Oil'
+                ]
+            }
+        ],
+        'newTitle': '',
+        'newIngredients':[]
     }
 
     render() {
@@ -13,17 +46,17 @@ export default class RecipeBook extends React.Component {
             <div className="container">
                 <ul className="nav nav-tabs">
                     <li className="nav-item">
-                        <button className="nav-link active" aria-current="page" onClick={()=>{
+                        <button className="nav-link active" aria-current="page" onClick={() => {
                             this.setActive('listing')
                         }}>
                             All Recipes
                         </button>
                     </li>
                     <li className="nav-item">
-                        <button className="nav-link" onClick={()=>{
+                        <button className="nav-link" onClick={() => {
                             this.setActive('addnew')
                         }}>Add New</button>
-                    </li>                 
+                    </li>
                 </ul>
                 {this.renderContent()}
             </div>
@@ -32,17 +65,43 @@ export default class RecipeBook extends React.Component {
     }
 
     renderContent() {
-        if (this.state.active=='listing') {
-            return <Listing/>
-        } else if (this.state.active=="addnew") {
-            return <AddNew/>
+        if (this.state.active == 'listing') {
+            return <Listing recipes={this.state.data}/>
+        } else if (this.state.active == "addnew") {
+            return <AddNew 
+                newTitle={this.state.newTitle}
+                newIngredients={this.state.newIngredients}
+                updateField = {this.updateFormField}
+                addNew={this.addNewRecipe}
+            />
         }
-
     }
 
     setActive = (page) => {
         this.setState({
             'active': page
+        })
+    }
+
+    updateFormField = (evt) => {
+        this.setState({
+            [evt.target.name] : evt.target.value
+        })
+    }
+
+    addNewRecipe = () => {
+        let newRecipe = {
+            '_id': Math.floor(Math.random() * 1000 + 9999),
+            'title': this.state.newTitle,
+            'ingredients': this.state.newIngredients.split(',')
+        }
+
+        // 1. clone the original array
+        // 2. modify the clone
+        // 3. replace the clone in the state
+        this.setState({
+            'data': [...this.state.data, newRecipe],
+            'active':'listing'
         })
     }
 }
